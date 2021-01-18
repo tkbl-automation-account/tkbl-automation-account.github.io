5888cc8e42c72107f89823eebfb1d5989361f82f
maxim <laba1203@gmail.com>
2021-01-18 14:35:22 +0200

    loyalty pages copyied for all envs

---
+++ bastion/loyalty/site_variables.js
@@ -0,0 +1,11 @@
+//Site variables:
+var site_data=function(){
+   var data = {
+        site_slug:"loyalty-automation-test-setup",
+        server:"//talkable.com",
+        tkbl_integration_scr:"//d2jjzw81hqbuqv.cloudfront.net/integration/clients/loyalty-automation-test-setup.min.js"
+    };
+   return data;
+}
+
+//End
