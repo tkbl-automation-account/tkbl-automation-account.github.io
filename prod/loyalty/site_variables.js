bf56517d56e1cdfc5775715314a76d6ece416409
tkbl-automation-account <42069506+tkbl-automation-account@users.noreply.github.com>
2021-01-18 10:42:02 +0200

    Create site_variables.js

---
+++ prod/loyalty/site_variables.js
@@ -0,0 +1,11 @@
+//Site variables:
+var site_data=function(){
+   var data = {
+        site_slug:"testmax-loyalty-17dec",
+        server:"//talkable.com",
+        tkbl_integration_scr:"integration.js"
+    };
+   return data;
+}
+
+//End
