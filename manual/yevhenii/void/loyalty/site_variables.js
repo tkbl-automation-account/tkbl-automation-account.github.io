9ff87232118027c5318097e92cb0559c11c93ed9
maxim <laba1203@gmail.com>
2021-01-22 13:05:25 +0200

    manual pages

---
+++ manual/yevhenii/void/loyalty/site_variables.js
@@ -0,0 +1,5 @@
+//Site variables:
+var site_data=function(){
+   var data = {"site_slug":"automation-site-142793939994","server":"//talkable.com","tkbl_integration_scr":"//d2jjzw81hqbuqv.cloudfront.net/integration/clients/automation-site-142793939994.min.js"};
+   return data;
+}
