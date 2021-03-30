ad216215af02b7c2fb0313804f5a68f7702a96d2
maxim <laba1203@gmail.com>
2021-03-30 18:15:06 +0300

    site added

---
+++ prod/full-integration/site_variables.js
@@ -0,0 +1,9 @@
+//Site variables:
+var site_data=function(){
+   var data = {
+   "site_slug":"testmax-full-integration-12mar",
+   "server":"//talkable.com",
+   "tkbl_integration_scr":"//di6re4dxelnn2.cloudfront.net/integration/clients/testmax-full-integration-12mar.min.js"};
+
+   return data;
+}
