5eac9de2b6d7187a221b114e9b790b51eaa1d621
tkbl-automation-account <42069506+tkbl-automation-account@users.noreply.github.com>
2021-04-09 14:59:23 +0000

    Update site_variables.js

--- void/manual/yevhenii/loyalty/site_variables.js
+++ void/manual/yevhenii/loyalty/site_variables.js
@@ -1,5 +1,5 @@
 //Site variables:
 var site_data=function(){
-   var data = {"site_slug":"automation-site-972730961791","server":"//void.talkable.com","tkbl_integration_scr":"//di6re4dxelnn2.cloudfront.net/integration/clients/automation-site-972730961791.min.js"};
+   var data = {"site_slug":"automation-site-980311487607","server":"//void.talkable.com","tkbl_integration_scr":"//di6re4dxelnn2.cloudfront.net/integration/clients/automation-site-980311487607.min.js"};
    return data;
 }
