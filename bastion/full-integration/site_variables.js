d9b6850f09e9b9a6fc056f02e4f5927cea5c4641
tkbl-automation-account <42069506+tkbl-automation-account@users.noreply.github.com>
2021-05-21 01:32:05 +0000

    Update site_variables.js

--- bastion/full-integration/site_variables.js
+++ bastion/full-integration/site_variables.js
@@ -1,5 +1,5 @@
 //Site variables:
 var site_data=function(){
-   var data = {"site_slug":"automation-site-387083882216","server":"//bastion.talkable.com","tkbl_integration_scr":"//di6re4dxelnn2.cloudfront.net/integration/clients/automation-site-387083882216.min.js"};
+   var data = {"site_slug":"automation-site-560686722930","server":"//bastion.talkable.com","tkbl_integration_scr":"//di6re4dxelnn2.cloudfront.net/integration/clients/automation-site-560686722930.min.js"};
    return data;
 }
