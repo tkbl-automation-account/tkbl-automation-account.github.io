49cea6c2caa20961d6a3cdedc2372d374cc02586
maxim <laba1203@gmail.com>
2021-05-26 16:27:24 +0300

    site added

--- void/full-integration/site_variables.js
+++ void/full-integration/site_variables.js
@@ -1,9 +1,9 @@
 //Site variables:
 var site_data=function(){
    var data = {
-   "site_slug":"testmax-full-integration-12mar",
+   "site_slug":"testmax-full-integration-26may",
    "server":"//void.talkable.com",
-   "tkbl_integration_scr":"//di6re4dxelnn2.cloudfront.net/integration/clients/testmax-full-integration-12mar.min.js"};
+   "tkbl_integration_scr":"//di6re4dxelnn2.cloudfront.net/integration/clients/testmax-full-integration-26may.min.js"};
 
    return data;
 }
