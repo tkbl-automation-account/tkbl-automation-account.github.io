3845b6b67c3fd70141f60c4ddf5c832a8628f1a1
maxim <laba1203@gmail.com>
2021-06-11 13:50:18 +0300

    rollback

--- void/loyalty/site_variables.js
+++ void/loyalty/site_variables.js
@@ -3,8 +3,7 @@ var site_data=function(){
    var data = {
         "site_slug":"automation-site-599893665742",
         "server":"//void.talkable.com",
-//        "tkbl_integration_scr":"//di6re4dxelnn2.cloudfront.net/integration/clients/automation-site-599893665742.min.js"
-        "tkbl_integration_scr":"integration.js"
+        "tkbl_integration_scr":"//di6re4dxelnn2.cloudfront.net/integration/clients/automation-site-599893665742.min.js"
    };
    return data;
 }
