575c43c452034fcb278f18ddb92f1d3ebd9686ea
maxim <laba1203@gmail.com>
2021-06-11 13:47:02 +0300

    custom integration

--- void/loyalty/site_variables.js
+++ void/loyalty/site_variables.js
@@ -1,5 +1,10 @@
 //Site variables:
 var site_data=function(){
-   var data = {"site_slug":"automation-site-599893665742","server":"//void.talkable.com","tkbl_integration_scr":"//di6re4dxelnn2.cloudfront.net/integration/clients/automation-site-599893665742.min.js"};
+   var data = {
+        "site_slug":"automation-site-599893665742",
+        "server":"//void.talkable.com",
+//        "tkbl_integration_scr":"//di6re4dxelnn2.cloudfront.net/integration/clients/automation-site-599893665742.min.js"
+        "tkbl_integration_scr":"integration.js"
+   };
    return data;
 }
