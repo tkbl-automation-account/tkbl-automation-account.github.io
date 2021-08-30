91eb0b4e4dd3e32e038798c25d0a2c45785ffbcc
tkbl-automation-account <42069506+tkbl-automation-account@users.noreply.github.com>
2021-08-30 14:38:08 +0000

    Update site_variables.js

--- bastion/manual/yevhenii/loyalty-tier/site_variables.js
+++ bastion/manual/yevhenii/loyalty-tier/site_variables.js
@@ -1,5 +1,5 @@
 //Site variables:
 var site_data=function(){
-   var data = {"site_slug":"automation-site-323319779259","server":"//bastion.talkable.com","tkbl_integration_scr":"//di6re4dxelnn2.cloudfront.net/integration/clients/automation-site-323319779259.min.js"};
+   var data = {"site_slug":"automation-site-33424799494","server":"//bastion.talkable.com","tkbl_integration_scr":"//di6re4dxelnn2.cloudfront.net/integration/clients/automation-site-33424799494.min.js"};
    return data;
 }
