982246564dd56649deec5b3dba7028d883b0f958
maxim <laba1203@gmail.com>
2021-11-22 15:42:49 +0200

    core integration

---
+++ prod/core-integration/site_variables.js
@@ -0,0 +1,10 @@
+//Site variables:
+var site_data=function(){
+   var data = {
+        site_slug:"testmax-loyalty-13oct2021",
+        server:"//void.talkable.com",
+        tkbl_integration_scr:"integration.js"
+    };
+   return data;
+}
+
