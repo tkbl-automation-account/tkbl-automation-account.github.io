1a67fb60047e357660e6c1ed012a6ba24fd40d3d
tkbl-automation-account <42069506+tkbl-automation-account@users.noreply.github.com>
2021-12-01 01:22:22 +0000

    Update site_variables.js

--- prod/core-integration/site_variables.js
+++ prod/core-integration/site_variables.js
@@ -1,5 +1,5 @@
 //Site variables:
 var site_data=function(){
-   var data = {"site_slug":"automation-site-199882390965","server":"//talkable.com","tkbl_integration_scr":"https://learn.talkable.com/vitali/releases/4.6.2/integration-new.js"};
+   var data = {"site_slug":"automation-site-321705273759","server":"//talkable.com","tkbl_integration_scr":"https://curebit.s3.amazonaws.com/integration/clients/automation-site-321705273759.min.js"};
    return data;
 }
