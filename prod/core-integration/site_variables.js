7f483b97d714f8cbfd6edd72575da5f7174c1d08
tkbl-automation-account <42069506+tkbl-automation-account@users.noreply.github.com>
2021-12-02 14:18:33 +0000

    Update site_variables.js

--- prod/core-integration/site_variables.js
+++ prod/core-integration/site_variables.js
@@ -1,5 +1,5 @@
 //Site variables:
 var site_data=function(){
-   var data = {"site_slug":"automation-site-382061643377","server":"//talkable.com","tkbl_integration_scr":"https://curebit.s3.amazonaws.com/integration/clients/automation-site-382061643377.min.js"};
+   var data = {"site_slug":"automation-site-454684157965","server":"//talkable.com","tkbl_integration_scr":"https://curebit.s3.amazonaws.com/integration/clients/automation-site-454684157965.min.js"};
    return data;
 }
