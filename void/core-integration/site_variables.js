ac3f2c08f4e1e627d72e6fb43316bfd31d80cccf
tkbl-automation-account <42069506+tkbl-automation-account@users.noreply.github.com>
2022-03-22 12:59:16 +0000

    Update site_variables.js

--- void/core-integration/site_variables.js
+++ void/core-integration/site_variables.js
@@ -1,5 +1,5 @@
 //Site variables:
 var site_data=function(){
-   var data = {"site_slug":"automation-site-952820558538","server":"//void.talkable.com","tkbl_integration_scr":"https://learn.talkable.com/pavel/one-call/lib.js"};
+   var data = {"site_slug":"automation-site-953895792890","server":"//void.talkable.com","tkbl_integration_scr":"https://learn.talkable.com/pavel/one-call/lib.js"};
    return data;
 }
