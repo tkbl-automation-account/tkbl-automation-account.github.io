3c7d39dcb8a7868cd9047cc16c0df6634636a40e
YevheniiKolesnyk <yevhenii.kolesnyk@gmail.com>
2022-03-31 18:05:46 +0300

    launch_campaigns: false

--- void/core-integration/init.js
+++ void/core-integration/init.js
@@ -34,7 +34,7 @@
 
     function tkbl_init(){
             window._talkableq = window._talkableq || [];
-            _talkableq.push(['init', { site_id: site_slug, server: server, email_capture_show_timeout: 1, launch_campaigns: false}]);
+            _talkableq.push(['init', { site_id: site_slug, server: server, email_capture_show_timeout: 1 }]);
     }
 
     function show_email_capture_offer(){
