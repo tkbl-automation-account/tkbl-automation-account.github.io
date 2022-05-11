607070e1eadbfec06d5b59a4b5e9ec9a9412d190
YevheniiKolesnyk <yevhenii.kolesnyk@gmail.com>
2022-05-11 18:39:26 +0300

    refactor new tkbl-site-new-5

--- new-approach/tkbl-site-new-5/page_builder.js
+++ new-approach/tkbl-site-new-5/page_builder.js
@@ -2,65 +2,79 @@
 //    build_nav_buttons();
 //};
 
-function build_page(){
-         build_integration_fields();
+function build_page() {
+    build_integration_fields();
 
-         var div = document.createElement("div");
-         var text = document.createTextNode("Site with new values");
-         div.appendChild(text);
-         document.getElementsByTagName('body')[0].appendChild(div);
+    var div = document.createElement("div");
+    var text = document.createTextNode("Site with new values");
+    div.appendChild(text);
+    document.getElementsByTagName('body')[0].appendChild(div);
 
-         populate_integration_values();
+    populate_integration_values();
+    build_integration_buttons();
+    build_nav_buttons();
+}
 
-         build_nav_buttons();
- }
+function build_nav_buttons() {
+    var query_params = '?site=' + site_data().site_slug + '&server=' + site_data().server_default + '&launch_campaigns=' + site_data().launch_campaigns;
+    addButton("Index", 'index.html' + query_params);
+    addButton("SA", 'sa.html' + query_params);
+    addButton("EC", 'ec.html' + query_params);
+    addButton("Loyalty", 'loyalty.html' + query_params);
+    addButton("Signup", 'signup.html' + query_params);
+    addButton("Post Purchase", 'pp.html' + query_params);
+    console.log('Navigation buttons were build');
+}
 
- function build_nav_buttons(){
-          addButton("Index", 'index.html');
-          addButton("SA", 'sa.html');
-          addButton("EC", 'ec.html');
-          addButton("Loyalty", 'loyalty.html');
-          addButton("Signup", 'signup.html');
-          addButton("Post Purchase", 'pp.html');
-          console.log('Navigation buttons were build');
-  }
-
- function build_integration_fields(){
+function build_integration_fields() {
     build_integration_field("Site slug: ", "site-slug");
     build_integration_field("Server: ", "server");
     build_integration_field("Integration: ", "integration-src");
- }
+}
 
- function populate_integration_values(){
-       document.getElementById("site-slug").value = site_data().site_slug;
-       document.getElementById("server").value = site_data().server;
-       document.getElementById("integration-src").value = site_data().tkbl_integration_scr;
- }
+function populate_integration_values() {
+    document.getElementById("site-slug").value = site_data().site_slug;
+    document.getElementById("server").value = site_data().server;
+    document.getElementById("integration-src").value = site_data().tkbl_integration_scr;
+}
 
- function build_integration_field(text, input_id){
-    var text = document.createTextNode(text);
+function build_integration_field(text, input_id) {
+    var text2 = document.createTextNode(text);
     var input = document.createElement("input");
     var br = document.createElement("br");
     input.id = input_id
     input.disabled = 'true'
     var element = document.getElementById("site-values");
-    element.appendChild(text);
+    element.appendChild(text2);
     element.appendChild(input);
     element.appendChild(br);
+}
 
- }
+function build_integration_buttons() {
+    add_integration_button('authenticate_customer', function () {authenticate_customer('')});
+    add_integration_button('register_affiliate', function () {register_affiliate()});
+    add_integration_button('show_loyalty', function () {show_loyalty()});
+    add_integration_button('show_email_capture_offer', function () {show_email_capture_offer()});
+}
 
-  function addButton(name, link){
-        var li = document.createElement("li"); // <p></p>
-        if(window.location.href.includes(link)){
-            li.class = 'active';
-        }
-        var a = document.createElement("a");
-        a.href = link;
-        var text = document.createTextNode(name);
-        a.appendChild(text);
-        li.appendChild(a);
+function add_integration_button(name, tlkb_function) {
+    const btn = document.createElement("button");
+    btn.innerHTML = name;
+    btn.onclick = tlkb_function;
+    document.body.appendChild(btn);
+}
 
-        var element = document.getElementById("nav_buttons");
-        element.appendChild(li);
-  }
+function addButton(name, link) {
+    var li = document.createElement("li");
+    if (window.location.href.includes(link)) {
+        li.class = 'active';
+    }
+    var a = document.createElement("a");
+    a.href = link;
+    var text = document.createTextNode(name);
+    a.appendChild(text);
+    li.appendChild(a);
+
+    var element = document.getElementById("nav_buttons");
+    element.appendChild(li);
+}
--- new-approach/tkbl-site-new-5/site_variables.js
+++ new-approach/tkbl-site-new-5/site_variables.js
@@ -1,55 +1,56 @@
+const queryParams = getUrlParams(window.location.search);
 
-    const queryParams = getUrlParams(window.location.search);
+var site = queryParams.site;
+var server = queryParams.server;
+var launch_campaigns = queryParams.launch_campaigns;
+var tkbl_integration = getIntegrationSrc(site);
 
-    var site = queryParams.site;
-    var server = queryParams.server;
-    var tkbl_integration = getIntegrationSrc(site)
-
-    function getIntegrationSrc(site){
-        var staging_prefix = '-staging';
-        if(server == 'prod'){
-            staging_prefix = '';
-        }
-        var src = "https://curebit" + staging_prefix + ".s3.amazonaws.com/integration/clients/" + queryParams.site + ".min.js";
-        console.log('Placements config loaded from: ' + src);
-        return src;
+function getIntegrationSrc(site) {
+    var staging_prefix = '-staging';
+    if (server === 'prod') {
+        staging_prefix = '';
     }
+    var src = "https://curebit" + staging_prefix + ".s3.amazonaws.com/integration/clients/" + queryParams.site + ".min.js";
+    console.log('Placements config loaded from: ' + src);
+    return src;
+}
 
-    function getServer(server) {
+function getServer(server) {
 
-        switch(server) {
-          case "void":
+    switch (server) {
+        case "void":
             return "//void.talkable.com";
             break;
-          case "bastion":
+        case "bastion":
             return "//bastion.talkable.com";
             break;
-          case "prod":
+        case "prod":
             return "//talkable.com";
 
-           default:
-           return server
-        }
-
+        default:
+            return server
     }
+}
 
-    function getUrlParams(search) {
-        const hashes = search.slice(search.indexOf('?') + 1).split('&')
-        const params = {}
-        hashes.map(hash => {
-            const [key, val] = hash.split('=')
-            params[key] = decodeURIComponent(val)
-            })
-            console.log(params)
-        return params
-    }
+function getUrlParams(search) {
+    const hashes = search.slice(search.indexOf('?') + 1).split('&')
+    const params = {}
+    hashes.map(hash => {
+        const [key, val] = hash.split('=')
+        params[key] = decodeURIComponent(val)
+    })
+    console.log(params)
+    return params
+}
 
-    var site_data=function(){
-        var data = {
-            site_slug: site,
-            server: getServer(server),
-            tkbl_integration_scr: tkbl_integration
-            //tkbl_integration_scr:"integration.js"
-        };
-        return data;
-    }
+var site_data = function () {
+    var data = {
+        site_slug: site,
+        server: getServer(server),
+        server_default: queryParams.server,
+        tkbl_integration_scr: tkbl_integration,
+        launch_campaigns: launch_campaigns
+        //tkbl_integration_scr:"integration.js"
+    };
+    return data;
+}
