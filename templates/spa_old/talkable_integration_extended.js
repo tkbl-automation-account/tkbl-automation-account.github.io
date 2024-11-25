8f174985666d815afddef994222e5117578aed79
maxim <laba1203@gmail.com>
2024-11-25 16:03:19 +0200

    update

--- templates/spa_old/talkable_integration_extended.js
+++ templates/spa_old/talkable_integration_extended.js
@@ -33,7 +33,7 @@ function insertAdditionalScripts(siteId, integrationScriptSrc) {
     var tkbl = document.createElement('script');
     tkbl.type = 'text/javascript';
     tkbl.async = true;
-    tkbl.src = 'https://tkbl-automation-account.github.io/integrations/spa_exclusions_fix.js';
+    tkbl.src = 'spa_exclusions_fix.js';
 //    tkbl.src = 'https://curebit-staging.s3.amazonaws.com/integration/clients/' + urlParams.get('site') + '.min.js';
 
     var s = document.getElementsByTagName('script')[0];
