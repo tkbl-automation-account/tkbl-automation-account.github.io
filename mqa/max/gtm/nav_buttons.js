b829838e06882632fb39b0f7a8e792718799d939
maxim <laba1203@gmail.com>
2024-11-26 15:17:53 +0200

    nav button updated

--- mqa/max/gtm/nav_buttons.js
+++ mqa/max/gtm/nav_buttons.js
@@ -5,16 +5,21 @@ window.dataLayer = window.dataLayer || [];
 dataLayer.push({
     'site': urlParams.get('site'),
     'server': urlParams.get('server')
+    'js_lib': urlParams.get('js_lib')
 });
 
 function build_nav_buttons() {
     const site = urlParams.get('site');
     const server = urlParams.get('server');
+    var js_lib;
+    if(urlParams.get('js_lib') != 'undefined'){
+        js_lib = '&' + urlParams.get('js_lib');
+    }
 
     const urls = {
-        'Index': `https://tkbl-automation-account.github.io/new-approach/gtm/index.html?site=${site}&server=${server}`,
-        'SA': `https://tkbl-automation-account.github.io/new-approach/gtm/sa.html?site=${site}&server=${server}`,
-        'Post Purchase': `https://tkbl-automation-account.github.io/new-approach/gtm/pp.html?site=${site}&server=${server}`
+        'Index': `https://tkbl-automation-account.github.io/new-approach/gtm/index.html?site=${site}&server=${server}${js_lib}`,
+        'SA': `https://tkbl-automation-account.github.io/new-approach/gtm/sa.html?site=${site}&server=${server}${js_lib}`,
+        'Post Purchase': `https://tkbl-automation-account.github.io/new-approach/gtm/pp.html?site=${site}&server=${server}${js_lib}`
     };
 
     const navButtons = document.getElementById('nav_buttons');
